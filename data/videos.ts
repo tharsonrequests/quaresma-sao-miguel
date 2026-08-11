16e1642e2ce4abef8be2c9b60687d39a4b54d801
tharsonrequests <tharson.requests@gmail.com>
2026-08-11 19:06:04 -0300

    Update videos.ts

--- data/videos.ts
+++ data/videos.ts
@@ -39,41 +39,41 @@ const LINKS: string[] = [
   "https://www.youtube.com/watch?v=uQzslqg3cio&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=37", // Dia 3
   "https://www.youtube.com/watch?v=S0RdtSLmJ4I&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=36", // Dia 4
   "https://www.youtube.com/watch?v=Jl4JCz5bMaY&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=35", // Dia 5
-  "", // Dia 6
-  "", // Dia 7
-  "", // Dia 8
-  "", // Dia 9
-  "", // Dia 10
-  "", // Dia 11
-  "", // Dia 12
-  "", // Dia 13
-  "", // Dia 14
-  "", // Dia 15
-  "", // Dia 16
-  "", // Dia 17
-  "", // Dia 18
-  "", // Dia 19
-  "", // Dia 20
-  "", // Dia 21
-  "", // Dia 22
-  "", // Dia 23
-  "", // Dia 24
-  "", // Dia 25
-  "", // Dia 26
-  "", // Dia 27
-  "", // Dia 28
-  "", // Dia 29
-  "", // Dia 30
-  "", // Dia 31
-  "", // Dia 32
-  "", // Dia 33
-  "", // Dia 34
-  "", // Dia 35
-  "", // Dia 36
-  "", // Dia 37
-  "", // Dia 38
-  "", // Dia 39
-  "", // Dia 40
+  "https://www.youtube.com/watch?v=QhI8lkx1ZqE&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=34", // Dia 6
+  "https://www.youtube.com/watch?v=j5E0YVNGgrk&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=33", // Dia 7
+  "https://www.youtube.com/watch?v=TKFh9Ey4IeI&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=32", // Dia 8
+  "https://www.youtube.com/watch?v=v7WumiqUaLc&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=31", // Dia 9
+  "https://www.youtube.com/watch?v=u7xXem0hJwA&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=30", // Dia 10
+  "https://www.youtube.com/watch?v=kOInW0Owb9s&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=29", // Dia 11
+  "https://www.youtube.com/watch?v=lBPMs1cbRlk&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=28", // Dia 12
+  "https://www.youtube.com/watch?v=914Hf72eBvg&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=27", // Dia 13
+  "https://www.youtube.com/watch?v=RMdg9x2EPhg&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=26", // Dia 14
+  "https://www.youtube.com/watch?v=xTqkluNeS48&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=25", // Dia 15
+  "https://www.youtube.com/watch?v=PuIi8-zIbE4&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=24", // Dia 16
+  "https://www.youtube.com/watch?v=iogb1Z2hWAQ&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=23", // Dia 17
+  "https://www.youtube.com/watch?v=AfoIDm4542U&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=22", // Dia 18
+  "https://www.youtube.com/watch?v=Kb3O_VXUYm0&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=21", // Dia 19
+  "https://www.youtube.com/watch?v=ezh08xnX9gw&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=20", // Dia 20
+  "https://www.youtube.com/watch?v=1_aq6MSuqLI&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=19", // Dia 21
+  "https://www.youtube.com/watch?v=zAzZklHcPYE&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=18", // Dia 22
+  "https://www.youtube.com/watch?v=FIKHjKzZDbc&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=17", // Dia 23
+  "https://www.youtube.com/watch?v=RsT_KV9v3xY&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=16", // Dia 24
+  "https://www.youtube.com/watch?v=SpH2Z0y9d_c&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=15", // Dia 25
+  "https://www.youtube.com/watch?v=riffSesPLc0&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=14", // Dia 26
+  "https://www.youtube.com/watch?v=Jmu8QEdFj4A&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=13", // Dia 27
+  "https://www.youtube.com/watch?v=Fasgxlul-6U&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=12", // Dia 28
+  "https://www.youtube.com/watch?v=hc-Mbrv7cjY&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=11", // Dia 29
+  "https://www.youtube.com/watch?v=Bu_Ys2ihjXo&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=10", // Dia 30
+  "https://www.youtube.com/watch?v=nWiMiqOktQQ&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=9", // Dia 31
+  "https://www.youtube.com/watch?v=XsBrPYEOtVg&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=8", // Dia 32
+  "https://www.youtube.com/watch?v=8xyInZxEcKE&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=7", // Dia 33
+  "https://www.youtube.com/watch?v=NyIV5XYvuHk&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=6", // Dia 34
+  "https://www.youtube.com/watch?v=s3iUXPmiesU&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=5", // Dia 35
+  "https://www.youtube.com/watch?v=HkexMEMtdkE&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=4", // Dia 36
+  "https://www.youtube.com/watch?v=uqpg_wW43mo&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=3", // Dia 37
+  "https://www.youtube.com/watch?v=5hHIyKhxw-s&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=2", // Dia 38
+  "https://www.youtube.com/watch?v=IccNQWfamUk&list=PLFHiit_0T6mnxqwHwQIih1104GPU8znLS&index=1", // Dia 39
+  "https://www.youtube.com/watch?v=kGVELI-Bgxg&t=4s", // Dia 40
 ]
 
 /** Títulos meditativos para cada um dos 40 dias. */
